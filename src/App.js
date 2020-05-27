5f5e5d3320634169c994bf09e85ef09aecca3aa8
A4K <khuranaamit122@gmail.com>
2020-05-27 14:30:03 -0700

    feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         New Try
+         Chala diya bc
         </p>
         <a
           className="App-link"
